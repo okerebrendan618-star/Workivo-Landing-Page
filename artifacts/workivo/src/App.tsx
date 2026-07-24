9d7302ad75cfce06c48f7a834107dea247a41c10
okerebrendan618-star <okerebrendan618@gmail.com>
2026-07-24 14:08:10 +0000

    Update App.tsx

--- artifacts/workivo/src/App.tsx
+++ artifacts/workivo/src/App.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { Route, Switch, Router as WouterRouter } from 'wouter';
 import Landing from '@/pages/Landing';
+import Signup from '@/pages/Signup';
 import NotFound from '@/pages/not-found';
+
 
 function App() {
   useEffect(() => {
@@ -11,9 +13,13 @@ function App() {
   return (
     <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, '')}>
       <Switch>
-        <Route path="/" component={Landing} />
-        <Route component={NotFound} />
-      </Switch>
+  <Route path="/" component={Landing} />
+  <Route path="/signup" component={Signup} />
+  <Route component={NotFound} />
+</Switch>
+        
+        
+      
     </WouterRouter>
   );
 }
